b3a6674c833fb4bfea22adb51e50a87741bb26aa
Mateusz Kwiatkowski <kwiat1990@gmail.com>
2022-12-14 14:05:32 +0100

    add entities

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use ggez::{conf, event, Context, GameResult};
-use specs::{Component, VecStorage, World, WorldExt};
+use specs::{Builder, Component, VecStorage, World, WorldExt};
 
 use std::path;
 
-#[derive(Debug, Component)]
+#[derive(Debug, Component, Clone, Copy)]
 #[storage(VecStorage)]
 pub struct Position {
     x: u8,
@@ -52,6 +52,60 @@ pub fn register_components(world: &mut World) {
     world.register::<Wall>();
     world.register::<Box>();
     world.register::<BoxSpot>();
+}
+
+pub fn create_wall(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "/images/wall.png".to_string(),
+        })
+        .with(Wall {})
+        .build();
+}
+
+pub fn create_floor(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 5, ..position })
+        .with(Renderable {
+            path: "/images/floor.png".to_string(),
+        })
+        .build();
+}
+
+pub fn create_box(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "/images/box.png".to_string(),
+        })
+        .with(Box {})
+        .build();
+}
+
+pub fn create_box_spot(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 9, ..position })
+        .with(Renderable {
+            path: "/images/box-spot.png".to_string(),
+        })
+        .with(BoxSpot {})
+        .build();
+}
+
+pub fn create_player(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "/images/player.png".to_string(),
+        })
+        .with(Player {})
+        .build();
 }
 
 fn main() {
